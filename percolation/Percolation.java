8e47ad2226d6332c8c5896d9307437cba5441c1b
Tamas Karacsony <tomik125@gmail.com>
2020-04-05 17:59:27 +0100

    96percent

--- percolation/Percolation.java
+++ percolation/Percolation.java
@@ -4,12 +4,19 @@
  *  Description:
  **************************************************************************** */
 
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+
 public class Percolation {
     private boolean[][] grid;
-    private int n;
-    private int len;
-    private WeightedQuickUnionFindwPathComp forprec;
-    private WeightedQuickUnionFindwPathComp forfull;
+    private final int n;
+    private int opensites = 0;
+    private final int len;
+    private final WeightedQuickUnionUF forprec;
+    private final WeightedQuickUnionUF forfull;
+    private int virtualtopperc;
+    private int virtualtopfull;
+    private int virtualbottom;
+
 
     // top id n*n bottom id n*n+1
     // creates n-by-n grid, with all sites initially blocked
@@ -20,8 +27,11 @@ public class Percolation {
         this.n = n;
         grid = new boolean[n][n];
         len = n * n + 2;
-        forprec = new WeightedQuickUnionFindwPathComp(len);
-        forfull = new WeightedQuickUnionFindwPathComp(len);
+        forprec = new WeightedQuickUnionUF(len);
+        forfull = new WeightedQuickUnionUF(len);
+        virtualtopperc = forprec.find(len - 2);
+        virtualtopfull = forfull.find(len - 2);
+        virtualbottom = forprec.find(len - 1);
     }
 
     // opens the site (row, col) if it is not open already
@@ -31,41 +41,46 @@ public class Percolation {
         }
 
         if (!grid[row - 1][col - 1]) {
+            opensites += 1;
             grid[row - 1][col - 1] = true;
-            int pos = (n * (row - 1) + (col - 1));
+            int percpos = forprec.find((n * (row - 1) + (col - 1)));
+            int fullpos = forfull.find((n * (row - 1) + (col - 1)));
+
             int posUp = (n * (row - 2) + (col - 1));
             int posDown = (n * (row) + (col - 1));
             int posRight = (n * (row - 1) + (col));
             int posLeft = (n * (row - 1) + (col - 2));
+            virtualtopperc = forprec.find(len - 2);
+            virtualtopfull = forfull.find(len - 2);
+            virtualbottom = forprec.find(len - 1);
             // implement up down left right union
             // union for top line the top id
             if (row != 1 && isOpen(row - 1, col)) {
-
-                forprec.union(forprec.ids[pos], forprec.ids[posUp]);
-                forfull.union(forfull.ids[pos], forfull.ids[posUp]);
+                forprec.union(percpos, forprec.find(posUp));
+                forfull.union(fullpos, forfull.find(posUp));
             }
             if (row != n && isOpen(row + 1, col)) {
-                forprec.union(forprec.ids[pos], forprec.ids[posDown]);
-                forfull.union(forfull.ids[pos], forfull.ids[posDown]);
+                forprec.union(percpos, forprec.find(posDown));
+                forfull.union(fullpos, forfull.find(posDown));
             }
 
             if (row == 1) {
-                forprec.union(forprec.ids[pos], forprec.ids[len - 2]);
-                forfull.union(forfull.ids[pos], forfull.ids[len - 2]);
+                forprec.union(percpos, virtualtopperc);
+                forfull.union(fullpos, virtualtopfull);
             }
 
             if (row == n) {
-                forprec.union(forprec.ids[pos], forprec.ids[len - 1]);
+                forprec.union(percpos, virtualbottom);
             }
 
             // left right
             if (col != 1 && isOpen(row, col - 1)) {
-                forprec.union(forprec.ids[pos], forprec.ids[posLeft]);
-                forfull.union(forfull.ids[pos], forfull.ids[posLeft]);
+                forprec.union(percpos, forprec.find(posLeft));
+                forfull.union(fullpos, forfull.find(posLeft));
             }
             if (col != n && isOpen(row, col + 1)) {
-                forprec.union(forprec.ids[pos], forprec.ids[posRight]);
-                forfull.union(forfull.ids[pos], forfull.ids[posRight]);
+                forprec.union(percpos, forprec.find(posRight));
+                forfull.union(fullpos, forfull.find(posRight));
             }
         }
     }
@@ -85,7 +100,7 @@ public class Percolation {
         }
         // if connected to top then full get id of it and connected?
         boolean full = false;
-        if (forfull.connected(forfull.ids[(n * (row - 1) + (col - 1))], forfull.ids[len - 2])) {
+        if (forfull.connected(forfull.find((n * (row - 1) + (col - 1))), virtualtopfull)) {
             full = true;
         }
 
@@ -94,27 +109,15 @@ public class Percolation {
 
     // returns the number of open sites
     public int numberOfOpenSites() {
-        int opensites = 0;
-        for (int row = 0; row < grid.length; row += 1) {
-            for (int col = 0; col < grid.length; col += 1) {
-                if (grid[row][col]) {
-                    opensites += 1;
-                }
-            }
-        }
         return opensites;
     }
 
     // does the system percolate?
     public boolean percolates() {
         boolean ispercolates = false;
-        if (forprec.connected(forprec.ids[n * n], forprec.ids[n * n + 1])) {
+        if (forprec.connected(virtualtopperc, virtualbottom)) {
             ispercolates = true;
         }
         return ispercolates;
-    }
-
-    public static void main(String[] args) {
-
     }
 }
--- percolation/PercolationStats.java
+++ percolation/PercolationStats.java
@@ -9,18 +9,19 @@ import edu.princeton.cs.algs4.StdRandom;
 import edu.princeton.cs.algs4.StdStats;
 
 public class PercolationStats {
-    // perform independent trials on an n-by-n grid
-    private double[] perctresh;
-    private int trials;
+    private final double mean;
+    private final double stddev;
+    private final double confidenceLo;
+    private final double confidenceHi;
 
     public PercolationStats(int n, int trials) {
         if (n <= 0 || trials <= 0) {
             throw new IllegalArgumentException();
         }
-        this.trials = trials;
         int gridend = n + 1;
         int totalsites = n * n;
-        perctresh = new double[trials];
+        // perform independent trials on an n-by-n grid
+        double[] perctresh = new double[trials];
         for (int i = 0; i < trials; i++) {
             Percolation perc = new Percolation(n);
             while (!perc.percolates()) {
@@ -29,54 +30,42 @@ public class PercolationStats {
             // store trial treshold for precolation open sites
             perctresh[i] = (double) perc.numberOfOpenSites() / (double) totalsites;
         }
-
+        mean = StdStats.mean(perctresh);
+        stddev = StdStats.stddev(perctresh);
+        double confidencefrac = (1.96 * stddev) / Math.sqrt(trials);
+        confidenceLo = mean - confidencefrac;
+        confidenceHi = mean + confidencefrac;
     }
 
     // sample mean of percolation threshold
     public double mean() {
-        return StdStats.mean(perctresh);
+        return mean;
     }
 
     // sample standard deviation of percolation threshold
     public double stddev() {
-        return StdStats.stddev(perctresh);
+        return stddev;
     }
 
     // low endpoint of 95% confidence interval
     public double confidenceLo() {
-        return mean() - 1.96 * Math.sqrt(stddev()) / Math.sqrt(trials);
+        return confidenceLo;
     }
 
     // high endpoint of 95% confidence interval
     public double confidenceHi() {
-        return mean() + 1.96 * Math.sqrt(stddev()) / Math.sqrt(trials);
+        return confidenceHi;
     }
 
     // test client (see below)
     public static void main(String[] args) {
         int n = Integer.parseInt(args[0]);
         int trials = Integer.parseInt(args[1]);
-        // if (args.length > 0) {
-        //     try {
-        //         n = Integer.parseInt(args[0]);
-        //     }
-        //     catch (NumberFormatException e) {
-        //         System.err.println("Argument" + args[0] + " must be an integer.");
-        //         System.exit(1);
-        //     }
-        //     try {
-        //         trials = Integer.parseInt(args[1]);
-        //     }
-        //     catch (NumberFormatException e) {
-        //         System.err.println("Argument" + args[1] + " must be an integer.");
-        //         System.exit(1);
-        //     }
-        // }
 
         PercolationStats ps = new PercolationStats(n, trials);
-        StdOut.println("mean = " + ps.mean());
-        StdOut.println("stddev = " + ps.stddev());
+        StdOut.println("mean = " + ps.mean);
+        StdOut.println("stddev = " + ps.stddev);
         StdOut.println(
-                "95% confidence interval = [" + ps.confidenceLo() + "," + ps.confidenceHi() + "]");
+                "95% confidence interval = [" + ps.confidenceLo + "," + ps.confidenceHi + "]");
     }
 }
